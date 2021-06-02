e27a98204da3d5e7d72c1f9537f61f8f2ef2e3a2
Abdelrhman-Gaber10 <84684295+Abdelrhman-Gaber10@users.noreply.github.com>
2021-06-02 22:45:24 +0200

    Update main.c

--- Project files/main.c
+++ Project files/main.c
@@ -13,3 +13,37 @@ void Led_Init (void){
     GPIO_PORTF_PCTL_R &= ~0x0000FFF0; // GPIO clear bit PCTL
     GPIO_PORTF_DATA_R &= ~0x0E; // initialize them to be off
 }
+#include <stdio.h>
+#include <math.h> 
+#define PI        3.14159265358979323846
+// Function to convert degrees to radii
+ double Radians(double degree)
+{
+	double onedeg = (PI) / 180;
+	return (onedeg * degree);
+}
+
+  double distance(double new_lat, double new_long, double old_lat, double old_long)
+{
+	// Converting latitude and longitude from the degree system to the radial system
+	new_lat = Radians(new_lat);
+	new_long = Radians(new_long);
+	old_lat = Radians(old_lat);
+	old_long = Radians(old_long);
+
+	// Haversine formula
+	 double deltalong = new_long - old_long;
+	 double deltalat = new_lat - old_lat;
+
+	long double ans = pow(sin(deltalat / 2), 2) + cos(new_lat) * cos(old_lat) * pow(sin(deltalong / 2), 2);
+
+	ans = 2 * asin(sqrt(ans));
+
+	// Earth's radius in metres
+	long double R = 6371000;
+
+	// Calculate the result
+	ans = ans * R;
+
+	return ans;
+}

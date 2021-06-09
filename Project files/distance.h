2a357004c65d6f806f1f7197777a3686cd338db9
Abdelrhman-Gaber10 <84684295+Abdelrhman-Gaber10@users.noreply.github.com>
2021-06-09 16:03:06 +0200

    Create distance.h

---
+++ Project files/distance.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to convert degrees to radii
+ long double Radians(long double degree){
+     long double onedeg = (PI) / 180;
+     return (onedeg * degree);
+}
+
+//  Function to calculate distance 
+long double Total_distance(long double new_lat,long double new_long,long double old_lat,long double old_long,long double current_distance){
+// Converting latitude and longitude from the degree system to the radial system
+     new_lat = Radians(new_lat);
+     new_long = Radians(new_long);
+     old_lat = Radians(old_lat);
+     old_long = Radians(old_long);
+// Haversine formula
+     long double deltalong = new_long - old_long;
+     long double deltalat = new_lat - old_lat;
+     long double ans = pow(sin(deltalat / 2), 2) + cos(new_lat) * cos(old_lat) * pow(sin(deltalong / 2), 2);
+     ans = 2 * asin(sqrt(ans));
+// Earth's radius in metres
+     long double R = 6371000;
+// Calculate the result
+	ans = ans * R;
+// updated distance
+        current_distance = ans + current_distance;
+	return (current_distance);
+}
